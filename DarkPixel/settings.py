5be17bc84ca6ac42ad34829082ee4ecb21331519
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2025-03-22 09:52:16 -0500

    makemigrations apps models

--- DarkPixel/settings.py
+++ DarkPixel/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Installer apps
-    'blog',
-    'home',
+    'apps.blog',
+    'apps.home',
 ]
 
 MIDDLEWARE = [
