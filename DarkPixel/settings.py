3a64438ceafc2c12eed2340244f8f2b1f9dcd02d
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2025-03-22 10:02:56 -0500

    load templates

--- DarkPixel/settings.py
+++ DarkPixel/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'DarkPixel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], # Load templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
